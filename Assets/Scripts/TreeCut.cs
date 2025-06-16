78e423b82568d5876c8447fa13ba7f09577194e1
MrAnonyms <raymond7c23@gmail.com>
2025-06-16 19:07:00 +0700

    Added Tree HP system
    Added HP slider for 1 tree, need more customizing, will finish tonight

--- Assets/Scripts/TreeCut.cs
+++ Assets/Scripts/TreeCut.cs
@@ -2,29 +2,40 @@ using UnityEngine;
 
 public class TreeCut : Tool // Make sure TreeCut still inherits from Tool
 {
-    [Header("Scoring")] // Keeps the Inspector organized
+    [Header("Tree Stats")]
+    [SerializeField] int treeHealth = 25; // The tree's starting health
+    [SerializeField] int damagePerHit = 10; // How much damage each hit does
+
+    [Header("Scoring")]
     [SerializeField] int pointsForCutting = 1; // How many points this tree gives when cut
 
-    // This 'Hit' method is called when the ToolController successfully "hits" this tree.
-    // It overrides the Hit() method from the base 'Tool' class.
+    // This 'Hit' method is called by the ToolController.
+    // It now handles health reduction before destroying the object.
     public override void Hit()
     {
-        Debug.Log(gameObject.name + " was hit! Adding score and removing object.");
+        // --- 1. Reduce Health ---
+        treeHealth -= damagePerHit;
+        Debug.Log(gameObject.name + " was hit! Remaining health: " + treeHealth);
 
-        // --- 1. Add Score ---
-        // This assumes you have the ScoreManager set up as we discussed before.
-        if (ScoreManager.Instance != null)
+        // --- 2. Check if Health is Depleted ---
+        // The rest of the code only runs if the tree's health is 0 or less.
+        if (treeHealth <= 0)
         {
-            ScoreManager.Instance.AddScore(pointsForCutting);
-        }
-        else
-        {
-            // This message is helpful if you forget to put the ScoreManager in your scene
-            Debug.LogError("ScoreManager.Instance is not found in the scene! Cannot add score. Make sure ScoreManagerObject is in your scene.");
-        }
+            Debug.Log(gameObject.name + " has been cut down! Adding score and removing object.");
 
-        // --- 2. Destroy the Tree GameObject ---
-        // This happens after scoring.
-        Destroy(gameObject);
+            // --- 3. Add Score ---
+            if (ScoreManager.Instance != null)
+            {
+                ScoreManager.Instance.AddScore(pointsForCutting);
+            }
+            else
+            {
+                Debug.LogError("ScoreManager.Instance is not found in the scene! Cannot add score.");
+            }
+
+            // --- 4. Destroy the Tree GameObject ---
+            // This now only happens when the tree runs out of health.
+            Destroy(gameObject);
+        }
     }
 }
